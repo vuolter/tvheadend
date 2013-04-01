474183be1cee268653d357eadc37d2f58072d304
Vuolter <vuolter@gmail.com>
2013-04-01 14:57:19 +0200

    Webui: cleanup and fixed some extensions loading

--- src/webui/extjs.c
+++ src/webui/extjs.c
@@ -98,7 +98,12 @@ extjs_root(http_connection_t *hc, const char *remain, void *opaque)
   htsbuf_qprintf(hq, "<script type=\"text/javascript\" src=\""EXTJSPATH"/adapter/ext/ext-base%s.js\"></script>\n"
                      "<script type=\"text/javascript\" src=\""EXTJSPATH"/ext-all%s.js\"></script>\n"
                      "<link rel=\"stylesheet\" type=\"text/css\" href=\""EXTJSPATH"/resources/css/ext-all-notheme%s.css\">\n"
+                     "<link rel=\"stylesheet\" type=\"text/css\" href=\"static/gridsearch/css/gridsearch.css\">\n"
                      "<link rel=\"stylesheet\" type=\"text/css\" href=\"static/livegrid/resources/css/ext-ux-livegrid.css\">\n"
+                     "<link rel=\"stylesheet\" type=\"text/css\" href=\"static/lovcombo/css/Ext.ux.form.LovCombo.css\">\n"
+                     "<link rel=\"stylesheet\" type=\"text/css\" href=\"static/multiselect/resources/multiselect.css\">\n"
+                     "<link rel=\"stylesheet\" type=\"text/css\" href=\"static/progresscolumn/resources/Ext.ux.grid.ProgressColumn.css\">\n"
+                     "<link rel=\"stylesheet\" type=\"text/css\" href=\"static/rowactions/css/Ext.ux.grid.RowActions.css\">\n"
                      "<link rel=\"stylesheet\" type=\"text/css\" href=\"static/app/ext.css\">\n",
                      tvheadend_webui_debug ? "-debug" : "",
                      tvheadend_webui_debug ? "-debug" : "",
@@ -110,11 +115,13 @@ extjs_root(http_connection_t *hc, const char *remain, void *opaque)
    * Load extjs extensions
    */
   extjs_load(hq, "static/app/extensions.js");
+  extjs_load(hq, "static/gridsearch/Ext.ux.grid.Search.js");
   extjs_load(hq, "static/livegrid/livegrid-all.js");
   extjs_load(hq, "static/lovcombo/Ext.ux.form.LovCombo.js");
-  extjs_load(hq, "static/multiselect/multiselect.js");
   extjs_load(hq, "static/multiselect/ddview.js");
-  extjs_load(hq, "static/gridsearch/Ext.ux.grid.Search.js");
+  extjs_load(hq, "static/multiselect/multiselect.js");
+  extjs_load(hq, "static/progresscolumn/Ext.ux.grid.ProgressColumn.js");
+  extjs_load(hq, "static/rowactions/Ext.ux.grid.RowActions.js");
 
   /**
    * Create a namespace for our app
