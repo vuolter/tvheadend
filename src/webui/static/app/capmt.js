7dc06e464cc19c7bffd46fc3bf544b9b76a102b2
Vuolter <vuolter@gmail.com>
2013-04-10 23:42:45 +0200

    Webui: cleanup capmt.js

--- src/webui/static/app/capmt.js
+++ src/webui/static/app/capmt.js
@@ -10,89 +10,94 @@ tvheadend.capmteditor = function() {
 		width : 250
 	});
 	
+	var actions = new Ext.ux.grid.RowActions({
+		actions : { iconIndex : 'connect', qtip : 'Connected' },
+		dataIndex : 'connected',
+		hideable : false,
+		width : 20
+	});
+	
 	var enabledColumn = new Ext.grid.CheckColumn({
-		header : "Enabled",
 		dataIndex : 'enabled',
-		width : 60
+		header : "Enabled",
+		hideable : false,
+		width : 80
 	});
-
+	
 	var oscamColumn = new Ext.grid.CheckColumn({
-		header : "OSCam mode",
 		dataIndex : 'oscam',
-		width : 60
+		header : "OSCam Mode",
+		width : 80
 	});
-
-	function setMetaAttr(meta, record) {
-		var enabled = record.get('enabled');
-		if (!enabled) return;
-
-		var connected = record.get('connected');
-		if (connected == 1) {
-			meta.attr = 'style="color:green;"';
-		}
-		else {
-			meta.attr = 'style="color:red;"';
-		}
-	}
-
+	
 	var sm = new Ext.grid.CheckboxSelectionModel();
 	
 	var cm = new Ext.grid.ColumnModel({
-  defaultSortable: true,
-  columns: [ sm, enabledColumn, {
-		header : "Camd.socket Filename",
-		dataIndex : 'camdfilename',
-		width : 200,
-		renderer : function(value, metadata, record, row, col, store) {
-			setMetaAttr(metadata, record);
-			return value;
-		},
-		editor : new Ext.form.TextField({
+		defaults : {
+			sortable : true,
 			allowBlank : false
-		})
-	}, {
-		header : "Listenport",
-		dataIndex : 'port',
-		renderer : function(value, metadata, record, row, col, store) {
-			setMetaAttr(metadata, record);
-			return value;
 		},
-		editor : new Ext.form.TextField({
-			allowBlank : false
-		})
-	}, oscamColumn, {
-		header : "Comment",
-		dataIndex : 'comment',
-		width : 400,
-		renderer : function(value, metadata, record, row, col, store) {
-			setMetaAttr(metadata, record);
-			return value;
-		},
-		editor : new Ext.form.TextField
-	} ]});
+		columns : [ actions, enabledColumn, {
+			dataIndex : 'camdfilename',
+			editor : new Ext.form.TextField,
+			header : "Camd.socket filename",
+			hideable : false,
+			renderer : function(value, metadata, record, row, col, store) {
+				return value ? value
+					: '<span class="tvh-grid-red">Unset</span>';
+			},
+			width : 400					
+		}, {
+			dataIndex : 'port',
+			editor : new Ext.form.TextField,
+			header : "Listen Port",
+			hideable : false,
+			renderer : function(value, metadata, record, row, col, store) {
+				return value ? value
+					: '<span class="tvh-grid-red">Unset</span>';
+			},
+			width : 100
+		}, 
+		oscamColumn, {
+			dataIndex : 'comment',
+			editor : new Ext.form.TextField({ allowBlank : true }),
+			header : "Comment",
+			renderer : function(value, metadata, record, row, col, store) {
+				return value ? value
+					: '<span class="tvh-grid-blue">No comments yet</span>';
+			},
+			width : 250
+		} ]
+	});
 
-	var rec = Ext.data.Record.create([ 'enabled', 'connected', 'camdfilename',
-		'port', 'oscam', 'comment' ]);
+	var rec = Ext.data.Record.create([ 'comment', 'connected', 'camdfilename', 'enabled', 'oscam', 'port' ]);
 
 	store = new Ext.data.JsonStore({
-		root : 'entries',
-		fields : rec,
-		url : "tablemgr",
 		autoLoad : true,
+		fields : rec,
 		id : 'id',
 		baseParams : {
 			table : 'capmt',
 			op : "get"
-		}
+		},
+		root : 'entries',
+		sortInfo : {
+			field : 'camdfilename',
+			direction : 'ASC'
+		},
+		url : "tablemgr"
 	});
 
+	var grid = new tvheadend.tableEditor('camptGrid', 'Capmt Connections', 'capmt', sm, cm, rec,
+		[ enabledColumn, oscamColumn, search ], store, 'config_capmt.html', 'key');
+		
 	tvheadend.comet.on('capmtStatus', function(server) {
 		var rec = store.getById(server.id);
 		if (rec) {
 			rec.set('connected', server.connected);
+			grid.getView().refresh();
 		}
 	});
-
-	return new tvheadend.tableEditor('camptGrid', 'Capmt Connections', 'capmt', sm, cm, rec,
-		[ enabledColumn, oscamColumn, search ], store, 'config_capmt.html', 'key');
+	
+	return grid;
 }
