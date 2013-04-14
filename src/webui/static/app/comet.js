4da9d79d94b9a0919fc066c5debadce22740183d
Vuolter <vuolter@gmail.com>
2013-04-14 19:23:49 +0200

    Webui: code cosmetics: comet.js

--- src/webui/static/app/comet.js
+++ src/webui/static/app/comet.js
@@ -22,37 +22,35 @@ tvheadend.boxid = null;
 
 tvheadend.cometPoller = function() {
 
-	var failures = 0;
-
 	var cometRequest = new Ext.util.DelayedTask(function() {
+		
+		var failures = 0;
+		
+		Ext.Ajax.request({
+			url : 'comet/poll',
+			params : {
+				boxid : (tvheadend.boxid ? tvheadend.boxid : null),
+				immediate : failures > 0
+			},
+			success : function(result, request) {
+				parse_comet_response(result.responseText);
 
-		Ext.Ajax
-			.request({
-				url : 'comet/poll',
-				params : {
-					boxid : (tvheadend.boxid ? tvheadend.boxid : null),
-					immediate : failures > 0 ? 1 : 0
-				},
-				success : function(result, request) {
-					parse_comet_response(result.responseText);
-
-					if (failures > 1) {
-						tvheadend.log('Reconnected to Tvheadend',
-							'font-weight: bold; color: #080');
-					}
-					failures = 0;
-				},
-				failure : function(result, request) {
-					cometRequest.delay(failures ? 1000 : 1);
-					if (failures == 1) {
-						tvheadend.log('There seems to be a problem with the '
-							+ 'live update feed from Tvheadend. '
-							+ 'Trying to reconnect...',
-							'font-weight: bold; color: #f00');
-					}
-					failures++;
+				if (failures > 1) {
+					tvheadend.log('Reconnected to Tvheadend', 'font-weight: bold; color: #080');
 				}
-			});
+				failures = 0;
+			},
+			failure : function(result, request) {
+				cometRequest.delay(failures ? 1000 : 1);
+				if (failures == 1) {
+					tvheadend.log('There seems to be a problem with the '
+						+ 'live update feed from Tvheadend. '
+						+ 'Trying to reconnect...',
+						'font-weight: bold; color: #f00');
+				}
+				++failures;
+			}
+		});
 	});
 
 	function parse_comet_response(responsetxt) {
@@ -64,7 +62,6 @@ tvheadend.cometPoller = function() {
 		}
 		cometRequest.delay(100);
 	}
-
 
 	cometRequest.delay(100);
 }
